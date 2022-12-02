66ccc319ce4c2d239d85edaf84374ae60c11a1cc
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-12-03 00:28:49 +0600

    redux install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
 function App() {
   return (
     <div>
+      <Provider >
       <RouterProvider router={routes} />
+      </Provider>
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,6 @@
-  import {createStore} from  "rudux";
-  
+  import {createStore} from "rudux";
+  import productReducer from "./reducers/productReducer";
+
+  const store = createStore(productReducer);
+
+  export default store;
